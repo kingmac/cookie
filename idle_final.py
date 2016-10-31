4b6c5a018cd9f76c3dbada8c3739eb1489c76a54
kingmac <17mccowkind@washk12.org>
2016-10-31 13:30:15 -0600

    Create idle_final.py
    ;)

---
+++ idle_final.py
@@ -0,0 +1,141 @@
+import time, random
+
+class Money:
+	def __init__(self, startingValue = 0):
+		self.value = startingValue
+
+	def get(self):
+		return self.value
+
+	def increase(self, amount):
+		self.value += amount
+
+	def decrease(self, amount):
+		if self.canAfford(amount):
+			self.value -= amount
+		else:
+			raise Exception("Can't afford")
+
+	def canAfford(self, amount):
+		return self.value >= amount
+
+
+
+class Building:
+	def __init__(self, name, cost, moneyPerSecond):
+		self.name = name
+		self.cost = cost
+		self.moneyPerSecond = moneyPerSecond
+		self.quantity = 0
+
+	def calcPerSecond(self):
+		return self.moneyPerSecond * self.quantity
+
+	def getData(self):
+		return self.name + ": " + str(self.cost) + ", " + str(self.moneyPerSecond) + ", " + str(self.quantity)
+
+
+
+class Clicker:
+	def __init__(self):
+		self.money = Money()
+		self.buildings = [
+			Building("Lemonade Stand", 50, 1),
+			Building("Arnold Palmer Stand", 5000, 10),
+			Building("Krusty Krab", 8478, 40),
+			Building("Bank", 10000, 70),
+			Building("Politics", 15000, 150)
+		]
+		self.clickerValue = 1
+		self.lastClick = time.time()
+
+	def run(self):
+		while True:
+			inp = input('> (' + str(self.getMoney()) + ') ')
+
+			self.doClick()
+
+			if inp == "quit":
+				self.save()
+				break
+
+			elif inp == "shop":
+				self.shop()
+				
+			else:
+				self.money.increase(self.clickerValue)
+
+	def shop(self):
+		while True:
+			print("\nMoney:", self.getMoney())
+			print()
+			for i in range(len(self.buildings)):
+				print(i+1, self.buildings[i].getData())
+
+			inp = input("Which one? ").strip()
+
+			self.doClick()
+
+			if inp != "":
+				try:
+					inp = int(inp)
+
+				except ValueError:
+					continue
+
+				print(inp)
+
+				if inp == 0:
+					break
+
+				else:
+					if inp > len(self.buildings):
+						continue
+
+					self.purchase(inp-1)
+
+	def purchase(self, index):
+		building = self.buildings[index]
+		cost = building.cost
+
+		if self.money.canAfford(cost):
+			self.money.decrease(cost)
+			building.quantity += 1
+			building.cost = int(cost * 1.1)
+
+		else:
+			print("\nNot enough money\n")
+
+	def calcMoneyPerSecond(self):
+		money = 0
+
+		for i in self.buildings:
+			money += i.calcPerSecond()
+
+		return money
+
+	def doClick(self):
+		current = time.time()
+		difference = current-self.lastClick
+		self.lastClick = current
+		self.money.increase(difference * self.calcMoneyPerSecond())
+
+	def getMoney(self):
+		return int(self.money.get())
+
+	def save(self):
+		pass
+
+
+
+def main():
+	clicker = Clicker()
+	clicker.run()
+
+
+
+
+
+
+if __name__ == "__main__":
+	main()
